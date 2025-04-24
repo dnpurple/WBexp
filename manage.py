5696319e6a3554decc6a238f8ac8b2dbbaeacd9b
purcell <purcell@uark.edu>
2025-04-24 14:38:25 -0500

    added redis and some stuff to settings.

--- manage.py
+++ manage.py
@@ -2,10 +2,13 @@
 import os
 import sys
 
-
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+    from otree.cli import cli
+    cli()
 
-    from otree.management.cli import execute_from_command_line  # pylint: disable=all
-
-    execute_from_command_line(sys.argv, script_file=__file__)
+#if __name__ == "__main__":
+ #   os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+  #  from otree.management.cli import execute_from_command_line
+   # execute_from_command_line(sys.argv)
+    #execute_from_command_line(sys.argv, script_file=__file__)
