6e4d52b318fc8e5ddbd9a19f75bdc75a8997df7d
purcell <purcell@uark.edu>
2025-04-26 13:12:25 -0500

    removed redis and other changes that wouldnt allow me to deploy. Redis stuff is still in the seetings

--- manage.py
+++ manage.py
@@ -4,11 +4,6 @@ import sys
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
-    from otree.cli import cli
-    cli()
-
-#if __name__ == "__main__":
- #   os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
-  #  from otree.management.cli import execute_from_command_line
-   # execute_from_command_line(sys.argv)
-    #execute_from_command_line(sys.argv, script_file=__file__)
+    from otree.management.cli import execute_from_command_line
+    execute_from_command_line(sys.argv)
+    execute_from_command_line(sys.argv, script_file=__file__)
